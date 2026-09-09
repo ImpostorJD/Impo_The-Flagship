ac469413131db4287a030ca8827e9b34785550b1
ImpostorJD <johndanieltejero23@gmail.com>
2026-09-09 20:47:43 +0800

    start flagship project

---
+++ flagship/src/main/java/com/the/flagship/jdt/flagship/FlagshipApplication.java
@@ -0,0 +1,13 @@
+package com.the.flagship.jdt.flagship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlagshipApplication.class, args);
+	}
+
+}
